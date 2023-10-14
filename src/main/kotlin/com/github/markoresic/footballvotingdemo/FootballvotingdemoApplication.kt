cf3dca6529fb8a7224ecbb6864696600e15d055e
Marko Rešić <mresic@racunarstvo.hr>
2023-10-14 18:46:30 +0200

    Initial commit

---
+++ src/main/kotlin/com/github/markoresic/footballvotingdemo/FootballvotingdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.markoresic.footballvotingdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FootballvotingdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<FootballvotingdemoApplication>(*args)
+}
